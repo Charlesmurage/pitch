93630c31df79330d62963ee223209bd75be12293
Charles maina <muragemaina09@gmail.com>
2019-05-27 11:56:47 +0300

    deploy to heroku

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/charles/Documents/Moringa-school-projects/pitch/virtual"
+set -gx VIRTUAL_ENV "/home/charles/Documents/Moringa-school-projects/watchlist/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
