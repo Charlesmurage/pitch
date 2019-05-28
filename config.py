972be693dd9a6b3bec28d96d41b9842a8025e72b
Charles maina <muragemaina09@gmail.com>
2019-05-28 12:38:29 +0300

    errors

--- config.py
+++ config.py
@@ -5,14 +5,7 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-  #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'pitch'
-    SENDER_EMAIL = 'muragemaina09@gmail.com'
+ 
   
 
 class ProdConfig(Config):
